8ce9af9a4d022f9d599bbea55fc4ae95c3acc76c
s0nicboOm <i.kushalbatra@gmail.com>
2022-07-19 16:31:59 -0700

    feat: add transformers model
    Signed-off-by: s0nicboOm <i.kushalbatra@gmail.com>

--- numalogic/models/autoencoder/variants/__init__.py
+++ numalogic/models/autoencoder/variants/__init__.py
@@ -1,7 +1,8 @@
 from numalogic.models.autoencoder.variants.vanilla import VanillaAE
 from numalogic.models.autoencoder.variants.conv import Conv1dAE
 from numalogic.models.autoencoder.variants.lstm import LSTMAE
+from numalogic.models.autoencoder.variants.transformer import TransformerAE
 from numalogic.models.autoencoder.base import TorchAE
 
 
-__all__ = ["VanillaAE", "Conv1dAE", "LSTMAE", "TorchAE"]
+__all__ = ["VanillaAE", "Conv1dAE", "LSTMAE", "TransformerAE", "TorchAE"]
