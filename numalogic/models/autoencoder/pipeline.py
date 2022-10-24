9b1c1176e29af75da0f5c712ed6e7b96b62204d5
Avik Basu <avikbasu93@gmail.com>
2022-10-24 16:27:21 -0400

    feat!: disentangle threshold selection from the main model
    Signed-off-by: Avik Basu <avikbasu93@gmail.com>

--- numalogic/models/autoencoder/pipeline.py
+++ numalogic/models/autoencoder/pipeline.py
@@ -1,12 +1,12 @@
 import io
 import logging
 from copy import copy
-from typing import Optional, Dict, Tuple, BinaryIO, Union, Callable
+from typing import Optional, Dict, BinaryIO, Union
 
 import numpy as np
 import torch
 from numpy.typing import NDArray
-from sklearn.base import OutlierMixin
+from sklearn.base import TransformerMixin, BaseEstimator
 from torch import nn, optim, Tensor
 from torch.utils.data import DataLoader
 
@@ -15,7 +15,7 @@ from numalogic.tools.types import AutoencoderModel
 _LOGGER = logging.getLogger(__name__)
 
 
-class AutoencoderPipeline(OutlierMixin):
+class AutoencoderPipeline(TransformerMixin, BaseEstimator):
     r"""
     Class to simplify training, inference, loading and saving of time-series autoencoders.
 
@@ -31,11 +31,6 @@ class AutoencoderPipeline(OutlierMixin):
         lr: learning rate
         batch_size: batch size for training
         num_epochs: number of epochs for training
-        std_tolerance: determines how many times the standard deviation to be used for threshold
-        reconerr_method: method used to calculate the distance
-                                between the original and the reconstucted data
-                                supported values include {"absolute", "squared"}
-        threshold_min: the minimum threshold to use;
                               can be used when the threshold calculated is too low
         resume_train: parameter to decide if resume training is needed. Also,
                               based on this parameter the optimizer state dict
@@ -59,10 +54,7 @@ class AutoencoderPipeline(OutlierMixin):
         lr: float = 0.001,
         batch_size: int = 256,
         num_epochs: int = 100,
-        std_tolerance: float = 3.0,
-        reconerr_method: str = "absolute",
-        threshold_min: float = None,
-        resume_train: bool = False,
+        resume_train: bool = False
     ):
         if not (model and seq_len):
             raise ValueError("No model and seq len provided!")
@@ -75,19 +67,13 @@ class AutoencoderPipeline(OutlierMixin):
         self.optimizer = self.init_optimizer(optimizer, lr)
         self.batch_size = batch_size
         self.num_epochs = num_epochs
-
-        self._thresholds = None
-        self._stats: Dict[str, Optional[float]] = dict(mean=None, std=None)
-        self.stdtol = std_tolerance
-        self.reconerr_func = self.get_reconerr_func(reconerr_method)
-        self.threshold_min = threshold_min
         self.resume_train = resume_train
 
     @property
     def model_properties(self):
         model_properties_dict = {
-            "thresholds": self._thresholds,
-            "err_stats": self._stats,
+            "batch_size": self.batch_size,
+            "num_epochs": self.num_epochs,
         }
         if self.resume_train:
             model_properties_dict["optimizer_state_dict"] = self.optimizer.state_dict()
@@ -96,22 +82,6 @@ class AutoencoderPipeline(OutlierMixin):
     @property
     def model(self) -> AutoencoderModel:
         return self._model
-
-    @property
-    def thresholds(self) -> Optional[NDArray[float]]:
-        return self._thresholds
-
-    @property
-    def err_stats(self) -> Dict[str, Optional[NDArray[float]]]:
-        return self._stats
-
-    @staticmethod
-    def get_reconerr_func(method: str) -> Callable:
-        if method == "squared":
-            return np.square
-        if method == "absolute":
-            return np.abs
-        raise ValueError(f"Unrecognized reconstruction error method specified: {method}")
 
     @staticmethod
     def init_criterion(loss_fn: str):
@@ -162,10 +132,6 @@ class AutoencoderPipeline(OutlierMixin):
                 _LOGGER.info(f"epoch : {epoch}, loss_mean : {np.mean(losses):.7f}")
             losses = []
 
-        self._thresholds, _mean, _std = self.find_thresholds(X)
-        self._stats["mean"] = _mean
-        self._stats["std"] = _std
-
         return self
 
     def predict(self, X: NDArray[float], seq_len: int = None) -> NDArray[float]:
@@ -187,59 +153,22 @@ class AutoencoderPipeline(OutlierMixin):
             _, pred = self._model(dataset.data)
         return dataset.recover_shape(pred)
 
-    def score(self, X: NDArray[float], seq_len: int = None) -> NDArray[float]:
-        r"""
-        Return anomaly score using the calculated threshold
-
-        Args:
-            X: training dataset
-            seq_len: sequence length / window length
-
-        Returns:
-            numpy array with anomaly scores
-        """
-        if self._thresholds is None:
-            raise RuntimeError("Thresholds not present!!!")
-        thresh = self._thresholds.reshape(1, -1)
-        if not seq_len:
-            seq_len = self.seq_len or len(X)
-        recon_err = self.recon_err(X, seq_len=seq_len)
-        anomaly_scores = recon_err / thresh
-        return anomaly_scores
-
-    def recon_err(self, X: NDArray[float], seq_len: int) -> NDArray:
+    def score(self, X: NDArray[float]) -> NDArray:
         r"""
         Returns the reconstruction error.
 
         Args:
-            X: training dataset
-            seq_len: sequence length / window length
+            X: data
 
         Returns:
             numpy array with anomaly scores
         """
-        x_recon = self.predict(X, seq_len=seq_len)
-        recon_err = self.reconerr_func(X - x_recon)
+        x_recon = self.predict(X, seq_len=self.seq_len)
+        recon_err = np.abs(X - x_recon)
         return recon_err
 
-    def find_thresholds(
-        self, X: NDArray[float]
-    ) -> Tuple[NDArray[float], NDArray[float], NDArray[float]]:
-        r"""
-        Calculate threshold for the anomaly model
-        Args:
-            X: training dataset
-
-        Returns:
-            Tuple consisting of thresholds, reconstruction error mean, reconstruction error std
-        """
-        recon_err = self.recon_err(X, seq_len=self.seq_len)
-        recon_err_mean = np.mean(recon_err, axis=0)
-        recon_err_std = np.std(recon_err, axis=0)
-        thresholds = recon_err_mean + (self.stdtol * recon_err_std)
-        if self.threshold_min:
-            thresholds[thresholds < self.threshold_min] = self.threshold_min
-        return thresholds, recon_err_mean, recon_err_std
+    def transform(self, X: NDArray[float]) -> NDArray:
+        return self.score(X)
 
     def save(self, path: Optional[str] = None) -> Optional[BinaryIO]:
         r"""
@@ -466,7 +395,3 @@ class SparseAEPipeline(AutoencoderPipeline):
 
             if epoch % log_freq == 0:
                 _LOGGER.info(f"epoch : {epoch}, penalty: {penalty} loss_mean : {loss.item():.7f}")
-
-        self._thresholds, _mean, _std = self.find_thresholds(X)
-        self._stats["mean"] = _mean
-        self._stats["std"] = _std
