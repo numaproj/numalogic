286695d807172e803f9da14f6db8381f0c1477e5
Avik Basu <ab93@users.noreply.github.com>
2023-01-05 15:43:34 -0800

    chore: add tests for thresholding
    Signed-off-by: Avik Basu <ab93@users.noreply.github.com>

--- numalogic/models/threshold/_std.py
+++ numalogic/models/threshold/_std.py
@@ -4,6 +4,15 @@ from sklearn.base import BaseEstimator
 
 
 class StdDevThreshold(BaseEstimator):
+    r"""
+    Threshold estimator that calculates based on the mean and the std deviation.
+
+    Threshold = Mean + (std_factor * Std)
+
+    Generates anomaly score as the ratio
+    between the input data and threshold generated.
+    """
+
     def __init__(self, std_factor: float = 3.0, min_threshold: float = 0.1):
         self.std_factor = std_factor
         self.min_threshold = min_threshold
@@ -24,14 +33,17 @@ class StdDevThreshold(BaseEstimator):
     def threshold(self):
         return self._threshold
 
-    def fit(self, X, y=None):
-        self._std = np.std(X, axis=0)
-        self._mean = np.mean(X, axis=0)
+    def fit(self, x_train: NDArray[float], y=None) -> "StdDevThreshold":
+        self._std = np.std(x_train, axis=0)
+        self._mean = np.mean(x_train, axis=0)
         self._threshold = self._mean + (self.std_factor * self._std)
         self._threshold[self._threshold < self.min_threshold] = self.min_threshold
 
         return self
 
-    def predict(self, X: NDArray[float]) -> NDArray[float]:
-        anomaly_scores = X / self.threshold
+    def predict(self, x_test: NDArray[float]) -> NDArray[float]:
+        anomaly_scores = x_test / self.threshold
         return anomaly_scores
+
+    def score(self, x_test: NDArray[float]) -> NDArray[float]:
+        return self.predict(x_test)
---
+++ tests/models/test_threshold.py
@@ -0,0 +1,27 @@
+import unittest
+
+import numpy as np
+
+from numalogic.models.threshold import StdDevThreshold
+
+
+class TestStdDevThreshold(unittest.TestCase):
+    def setUp(self) -> None:
+        self.x_train = np.arange(100).reshape(50, 2)
+        self.x_test = np.arange(100, 160, 6).reshape(5, 2)
+
+    def test_estimator_predict(self):
+        clf = StdDevThreshold()
+        clf.fit(self.x_train)
+        score = clf.predict(self.x_test)
+        self.assertAlmostEqual(0.93317, np.mean(score), places=2)
+
+    def test_estimator_score(self):
+        clf = StdDevThreshold()
+        clf.fit(self.x_train)
+        score = clf.score(self.x_test)
+        self.assertAlmostEqual(0.93317, np.mean(score), places=2)
+
+
+if __name__ == "__main__":
+    unittest.main()
