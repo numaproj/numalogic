47d88e9c6236af566b8b2e1087bb60ff02214d38
Avik Basu <avik_basu@intuit.com>
2023-04-26 12:55:53 -0700

    fix lint
    Signed-off-by: Avik Basu <avik_basu@intuit.com>

--- numalogic/config/_config.py
+++ numalogic/config/_config.py
@@ -50,6 +50,7 @@ class LightningTrainerConf:
     More details on the arguments are provided here:
     https://pytorch-lightning.readthedocs.io/en/stable/common/trainer.html#trainer-class-api
     """
+
     accelerator: str = "auto"
     max_epochs: int = 100
     logger: bool = False
