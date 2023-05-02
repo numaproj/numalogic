89a6b5d660be07c2cb4fc8ffe9449c0dbd531081
Avik Basu <3485425+ab93@users.noreply.github.com>
2023-05-02 12:05:47 -0700

    feat: first benchmarking using KPI anomaly data (#163)
    Signed-off-by: Avik Basu <ab93@users.noreply.github.com>

--- numalogic/config/_config.py
+++ numalogic/config/_config.py
@@ -51,6 +51,7 @@ class LightningTrainerConf:
     https://pytorch-lightning.readthedocs.io/en/stable/common/trainer.html#trainer-class-api
     """
 
+    accelerator: str = "auto"
     max_epochs: int = 100
     logger: bool = False
     check_val_every_n_epoch: int = 5
