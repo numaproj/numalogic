4411aa7f0f762a450a925c9546eb66be7de02a55
Avik Basu <3485425+ab93@users.noreply.github.com>
2023-06-02 12:58:09 -0700

    examples: update with new numalogic and pynumaflow (#202)
    - update examples for numalogic version and pynumaflow versions
    - base numalogic udf
    - pynumaflow as an optional dependency
    - class-based udfs
    - remove protobuf 3.20 requirement
    
    ---------
    
    Signed-off-by: Avik Basu <ab93@users.noreply.github.com>

--- examples/numalogic-simple-pipeline/src/udf/preprocess.py
+++ examples/numalogic-simple-pipeline/src/udf/preprocess.py
@@ -2,8 +2,8 @@ import json
 import logging
 import uuid
 
-import numpy as np
 from numalogic.transforms import LogTransformer
+from numalogic.numaflow import NumalogicUDF
 from pynumaflow.function import Messages, Message, Datum
 
 from src.utils import Payload
@@ -11,23 +11,29 @@ from src.utils import Payload
 LOGGER = logging.getLogger(__name__)
 
 
-def preprocess(_: str, datum: Datum) -> Messages:
-    r"""The preprocess function here transforms the input data for ML inference and sends
-    the payload to inference vertex.
+class Preprocess(NumalogicUDF):
+    """UDF to preprocess the input data for ML inference."""
 
-    For more information about the arguments, refer:
-    https://github.com/numaproj/numaflow-python/blob/main/pynumaflow/function/_dtypes.py
-    """
-    # Load json data
-    json_data = datum.value
-    ts_array = json.loads(json_data)["data"]
-    payload = Payload(ts_data=ts_array, uuid=str(uuid.uuid4()))
+    def __init__(self):
+        super().__init__()
 
-    # preprocess step
-    data = np.asarray(payload.ts_data)
-    clf = LogTransformer()
-    payload.ts_data = clf.transform(data).tolist()
-    LOGGER.info("%s - Preprocess complete for data: %s", payload.uuid, payload.ts_data)
+    def exec(self, _: list[str], datum: Datum) -> Messages:
+        """The preprocess function here transforms the input data for ML inference and sends
+        the payload to inference vertex.
 
-    # Convert Payload back to bytes
-    return Messages(Message.to_all(payload.to_json().encode("utf-8")))
+        For more information about the arguments, refer:
+        https://github.com/numaproj/numaflow-python/blob/main/pynumaflow/function/_dtypes.py
+        """
+        # Load json data
+        series = json.loads(datum.value)["data"]
+        payload = Payload(uuid=str(uuid.uuid4()), arr=series)
+
+        # preprocess step
+        data = payload.get_array()
+        clf = LogTransformer()
+        out = clf.fit_transform(data)
+        payload.set_array(out.tolist())
+        LOGGER.info("%s - Preprocess complete for data: %s", payload.uuid, payload.arr)
+
+        # Return as a Messages object
+        return Messages(Message(value=payload.to_json()))
---
+++ tests/test_numaflow.py
@@ -0,0 +1,81 @@
+import unittest
+from datetime import datetime
+
+from pynumaflow.function import Datum, Messages, Message
+from pynumaflow.function._dtypes import DatumMetadata
+
+from numalogic.numaflow import NumalogicUDF
+
+
+class DummyUDF(NumalogicUDF):
+    def __init__(self):
+        super().__init__()
+
+    def exec(self, keys: list[str], datum: Datum) -> Messages:
+        val = datum.value
+        return Messages(Message(value=val, keys=keys))
+
+
+class DummyAsyncUDF(NumalogicUDF):
+    def __init__(self):
+        super().__init__(is_async=True)
+
+    async def aexec(self, keys: list[str], datum: Datum) -> Messages:
+        val = datum.value
+        return Messages(Message(value=val, keys=keys))
+
+
+class TestNumalogicUDF(unittest.TestCase):
+    def setUp(self) -> None:
+        self.datum = Datum(
+            keys=["k1", "k2"],
+            value=b"100",
+            event_time=datetime.now(),
+            watermark=datetime.now(),
+            metadata=DatumMetadata("1", 0),
+        )
+
+    def test_exec(self):
+        udf = DummyUDF()
+        msgs = udf.exec(["key1", "key2"], self.datum)
+        self.assertIsInstance(msgs, Messages)
+
+    def test_call(self):
+        udf = DummyUDF()
+        msgs = udf(["key1", "key2"], self.datum)
+        self.assertIsInstance(msgs, Messages)
+
+    async def test_aexec(self):
+        udf = DummyUDF()
+        with self.assertRaises(NotImplementedError):
+            await udf.aexec(["key1", "key2"], self.datum)
+
+
+class TestNumalogicAsyncUDF(unittest.IsolatedAsyncioTestCase):
+    def setUp(self) -> None:
+        self.datum = Datum(
+            keys=["k1", "k2"],
+            value=b"100",
+            event_time=datetime.now(),
+            watermark=datetime.now(),
+            metadata=DatumMetadata("1", 0),
+        )
+
+    async def test_aexec(self):
+        udf = DummyAsyncUDF()
+        msgs = await udf.aexec(["key1", "key2"], self.datum)
+        self.assertIsInstance(msgs, Messages)
+
+    async def test_call(self):
+        udf = DummyAsyncUDF()
+        msgs = await udf(["key1", "key2"], self.datum)
+        self.assertIsInstance(msgs, Messages)
+
+    def test_exec(self):
+        udf = DummyAsyncUDF()
+        with self.assertRaises(NotImplementedError):
+            udf.exec(["key1", "key2"], self.datum)
+
+
+if __name__ == "__main__":
+    unittest.main()
