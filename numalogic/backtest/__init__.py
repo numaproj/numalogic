0ffba8aec82b95641d740a365f2bdda18e61ed3e
Avik Basu <ab93@users.noreply.github.com>
2023-09-07 16:34:50 -0700

    try improving packaging
    Signed-off-by: Avik Basu <ab93@users.noreply.github.com>

--- numalogic/backtest/__init__.py
+++ numalogic/backtest/__init__.py
@@ -1,7 +1,11 @@
-try:
-    import torch
-    import pytorch_lightning
-except ModuleNotFoundError:
-    raise ModuleNotFoundError(
-        "Pytorch and/or Pytorch lightning is not installed. Please install it first."
-    ) from None
+from importlib.util import find_spec
+
+
+def _validate_req_pkgs():
+    if (not find_spec("torch")) or (not find_spec("pytorch_lightning")):
+        raise ModuleNotFoundError(
+            "Pytorch and/or Pytorch lightning is not installed. Please install them first."
+        )
+
+
+_validate_req_pkgs()
