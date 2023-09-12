ddc3c9876004be77a48eae1ed74f8b72650cb18e
Shrivardhan Rao <Shrivardhan_Rao@intuit.com>
2023-09-12 16:23:58 -0700

    feat: use well-defined dimensions instead of strings
    Signed-off-by: Shrivardhan Rao <Shrivardhan_Rao@intuit.com>

--- numalogic/connectors/druid.py
+++ numalogic/connectors/druid.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta
 import pandas as pd
 import pytz
 from pydruid.client import PyDruid
+from pydruid.utils.dimensions import DimensionSpec
 from pydruid.utils.filters import Filter
 from numalogic.connectors._config import Pivot
 from typing import Optional
@@ -50,13 +51,17 @@ class DruidFetcher:
         start_dt = end_dt - timedelta(hours=hours)
         intervals = f"{start_dt.isoformat()}/{end_dt.isoformat()}"
 
+        dimension_specs = []
+        for d in dimensions:
+            dimension_specs.append(DimensionSpec(dimension=d, output_name=d))
+
         params = {
             "datasource": datasource,
             "granularity": granularity,
             "intervals": intervals,
             "aggregations": aggregations,
             "filter": _filter,
-            "dimensions": dimensions,
+            "dimensions": dimension_specs,
         }
 
         _LOGGER.debug(
