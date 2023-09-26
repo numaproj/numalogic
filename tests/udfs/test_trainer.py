73a5c3a8df23997197b0244d056dc17143a2df5a
s0nicboOm <i.kushalbatra@gmail.com>
2023-09-26 11:47:28 -0700

    test-wfl-error
    Signed-off-by: s0nicboOm <i.kushalbatra@gmail.com>

--- tests/udfs/test_trainer.py
+++ tests/udfs/test_trainer.py
@@ -210,8 +210,10 @@ class TrainTrainerUDF(unittest.TestCase):
         with freeze_time(datetime.now() + timedelta(hours=25)):
             TrainMsgDeduplicator(REDIS_CLIENT).ack_read(self.keys, "some-uuid")
             ts = datetime.now()
+            print(datetime.now())
         with freeze_time(ts + timedelta(minutes=15)):
             self.udf(self.keys, self.datum)
+            print(datetime.now())
         print(REDIS_CLIENT.keys())
         self.assertEqual(
             0,
