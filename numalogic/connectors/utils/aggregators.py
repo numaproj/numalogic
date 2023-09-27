a8e92a44789d60807d4647ce3addf47e63f77f10
Shrivardhan Rao <Shrivardhan_Rao@intuit.com>
2023-09-26 19:05:16 -0700

    use local utils
    Signed-off-by: Shrivardhan Rao <Shrivardhan_Rao@intuit.com>

--- numalogic/connectors/utils/aggregators.py
+++ numalogic/connectors/utils/aggregators.py
@@ -4,10 +4,10 @@ def quantiles_doubles_sketch(
     """
 
     Args:
-        raw_column: name of the column in druid
-        aggregator_name: arbitrary aggregator name
-        k: controls accuracy, higher the better.  Must be a power of 2 from 2 to 32768
-        max_stream_length: this parameter defines the number of items that can be presented to
+        raw_column: Name of the column in druid
+        aggregator_name: Arbitrary aggregator name
+        k: Controls accuracy, higher the better.  Must be a power of 2 from 2 to 32768
+        max_stream_length: This parameter defines the number of items that can be presented to
         each sketch before it may need to move from off-heap to on-heap memory.
 
     Returns: quantilesDoublesSketch aggregator dict
