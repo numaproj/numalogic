af8f28c66986d8cb30b5d2512c8dc4590a655d9c
s0nicboOm <i.kushalbatra@gmail.com>
2023-11-13 12:09:12 -0800

    fix: add typing
    Signed-off-by: s0nicboOm <i.kushalbatra@gmail.com>

--- numalogic/udfs/trainer.py
+++ numalogic/udfs/trainer.py
@@ -323,7 +323,7 @@ class TrainerUDF(NumalogicUDF):
     def _is_data_sufficient(self, payload: TrainerPayload, df: pd.DataFrame) -> bool:
         _conf = self.get_conf(payload.config_id)
         if len(df) < _conf.numalogic_conf.trainer.min_train_size:
-            self.train_msg_deduplicator.ack_insufficient_data(
+            _ = self.train_msg_deduplicator.ack_insufficient_data(
                 key=payload.composite_keys, uuid=payload.uuid, train_records=len(df)
             )
             return False
