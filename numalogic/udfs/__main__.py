9b4277f5a95fbdabb93cece6c5b41a9e281e81e5
Nandita Koppisetty <nandita.iitkgp@gmail.com>
2023-12-12 16:37:14 +0530

    fix: server start
    Signed-off-by: Nandita Koppisetty <nandita.iitkgp@gmail.com>

--- numalogic/udfs/__main__.py
+++ numalogic/udfs/__main__.py
@@ -22,7 +22,10 @@ def init_server(step: str, server_type: str):
     LOGGER.info("Pipeline config: %s", pipeline_conf)
 
     redis_client = get_redis_client_from_conf(pipeline_conf.redis_conf)
-    udf = UDFFactory.get_udf_instance(step, r_client=redis_client, pl_conf=pipeline_conf)
+    if step == "mlpipeline":
+        udf = UDFFactory.get_udf_instance(step, pl_conf=pipeline_conf)
+    else:
+        udf = UDFFactory.get_udf_instance(step, r_client=redis_client, pl_conf=pipeline_conf)
 
     return ServerFactory.get_server_instance(server_type, handler=udf)
 
