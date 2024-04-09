fix: overflow for static threshold
Signed-off-by: Avik Basu <ab93@users.noreply.github.com>

numalogic/models/threshold/_static.py
@@ -87,9 +87,11 @@ class SigmoidThreshold(BaseThresholdModel):
         ValueError: If the input data shape does not match the provided upper_limits
     """
 
+    _CLIP_VALUE: int = 80
+
     __slots__ = ("upper_limits", "coeff", "score_limit")
 
-    def __init__(self, *upper_limits: float, slope_factor: int = 5, score_limit: int = 10):
+    def __init__(self, *upper_limits: float, slope_factor: int = 5, score_limit: float = 10.0):
         self.upper_limits = np.asarray(upper_limits, dtype=np.float32)
         self.coeff = slope_factor * np.pi
         self.score_limit = score_limit
@@ -121,4 +123,7 @@ class SigmoidThreshold(BaseThresholdModel):
         with values being anomaly scores.
         """
         self._validate_input(x)
-        return self.score_limit / (1.0 + np.exp(-self.coeff * (x.copy() - self.upper_limits)))
+        exp_arg = np.clip(
+            -self.coeff * (x - self.upper_limits), -self._CLIP_VALUE, -self._CLIP_VALUE
+        ).astype(np.float32)
+        return self.score_limit / (1.0 + np.exp(exp_arg))

tests/models/threshold/test_static.py
@@ -1,7 +1,13 @@
+import logging
+
 import pytest
 import numpy as np
+from numpy.testing import assert_almost_equal
 
 from numalogic.models.threshold import StaticThreshold, SigmoidThreshold
+
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 @pytest.fixture
@@ -27,17 +33,19 @@ def test_static_threshold_score_01(x):
 
 
 def test_sigmoid_threshold_score_02(x):
-    clf = SigmoidThreshold(5, 10)
+    clf = SigmoidThreshold(2.0, 1e100)
     y = clf.score_samples(x)
     assert x.shape == y.shape
-    assert np.max(y) == clf.score_limit
-    assert np.min(y) > 0.0
+    assert_almost_equal(np.max(y, axis=0)[0], clf.score_limit)
+    assert_almost_equal(np.max(y, axis=0)[1], 0.0)
+    assert y.dtype == np.float32
 
 
 def test_sigmoid_threshold_predict(x):
     clf = SigmoidThreshold(5)
     clf.fit(x)
     y = clf.predict(x)
+
     assert x.shape == y.shape
     assert np.max(y) == 1
     assert np.min(y) == 0