e03df9ac899e5513a1932a97fac7e768f67626c9
Avik Basu <ab93@users.noreply.github.com>
2024-06-04 15:15:32 -0700

    fix: log file
    Signed-off-by: Avik Basu <ab93@users.noreply.github.com>

--- apps/numa-udfs/src/constants.py
+++ apps/numa-udfs/src/constants.py
@@ -8,6 +8,6 @@ BASE_CONF_DIR = os.path.join(BASE_DIR, "config")
 
 DEFAULT_BASE_CONF_PATH = os.path.join(BASE_CONF_DIR, "default-configs", "config.yaml")
 DEFAULT_APP_CONF_PATH = os.path.join(BASE_CONF_DIR, "app-configs", "config.yaml")
-LOG_CONF_PATH = os.path.join(SRC_DIR, "log.conf")
+LOG_CONF_PATH = os.path.join(BASE_DIR, "log.conf")
 DEFAULT_METRICS_PORT = 8490
 NUMALOGIC_METRICS = "numalogic_metrics"
