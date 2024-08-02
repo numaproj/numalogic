4007e19030777d67d264fd1bbf9c6deb386eac63
Kushal Batra <i.kushalbatra@gmail.com>
2024-08-02 15:17:16 -0700

    fix : typing
    Signed-off-by: Kushal Batra <i.kushalbatra@gmail.com>

--- numalogic/models/forecast/variants/xgboost.py
+++ numalogic/models/forecast/variants/xgboost.py
@@ -1,5 +1,5 @@
 import logging
-from typing import ClassVar, Optional
+from typing import Optional
 
 import numpy as np
 import pandas as pd
@@ -35,14 +35,14 @@ class XGBoostForecaster:
         regressor_params: additional parameters for the XGBoost regressor
     """
 
-    __slots__: ClassVar = [
-        "horizon",
-        "seq_len",
-        "val_split",
-        "model",
-        "early_stop_callback",
-        "early_stopping",
-    ]
+    __slots__ = (
+        "_horizon",
+        "_seq_len",
+        "_val_split",
+        "_model",
+        "_early_stop_callback",
+        "_early_stopping",
+    )
 
     def __init__(
         self,
@@ -52,12 +52,12 @@ class XGBoostForecaster:
         early_stopping=True,
         val_split: float = 0.1,
     ):
-        self.horizon = horizon
-        self.seq_len = seq_len
-        self.val_split: ClassVar = 0
-        self.early_stopping = early_stopping
+        self._horizon = horizon
+        self._seq_len = seq_len
+        self._val_split = 0
+        self._early_stopping = early_stopping
         if early_stopping:
-            self.val_split = val_split
+            self._val_split = val_split
         early_stop_callback = callback.EarlyStopping(
             rounds=20, metric_name="rmse", save_best=True, maximize=False, min_delta=1e-4
         )
@@ -81,7 +81,7 @@ class XGBoostForecaster:
         if regressor_params:
             default_params.update(default_params)
 
-        self.model: ClassVar = XGBRegressor(**default_params)
+        self._model = XGBRegressor(**default_params)
 
     def prepare_data(self, x: np.array):
         """
@@ -93,11 +93,11 @@ class XGBoostForecaster:
             seq_len: int: sequence length
             horizon: int: forecast horizon
         """
-        ds = ForecastDataset(x, seq_len=self.seq_len, horizon=self.horizon)
+        ds = ForecastDataset(x, seq_len=self._seq_len, horizon=self._horizon)
         dataloaders = DataLoader(ds, batch_size=1)
 
-        X = np.empty((0, self.seq_len, x.shape[1]))
-        Y = np.empty((0, self.horizon, 1))
+        X = np.empty((0, self._seq_len, x.shape[1]))
+        Y = np.empty((0, self._horizon, 1))
         for x, y in dataloaders:
             X = np.concatenate([X, x.numpy()], axis=0)
             Y = np.concatenate([Y, y[:, :, 0].unsqueeze(-1).numpy()], axis=0)
@@ -109,8 +109,8 @@ class XGBoostForecaster:
         _check_data_format(df)
 
         # Split the data into training and validation sets
-        train_df = df.iloc[: int(len(df) * (1 - self.val_split)), :]
-        val_df = df.iloc[int(len(df) * (1 - self.val_split)) :, :] if self.val_split else None
+        train_df = df.iloc[: int(len(df) * (1 - self._val_split)), :]
+        val_df = df.iloc[int(len(df) * (1 - self._val_split)) :, :] if self._val_split else None
 
         # Transform and prepare the training data
         transformed_train_data = CovariatesGenerator().transform(train_df)
@@ -121,31 +121,31 @@ class XGBoostForecaster:
             transformed_val_data = CovariatesGenerator().transform(val_df)
             x_val, y_val = self.prepare_data(transformed_val_data)
             _LOGGER.info("Fitting the model with validation data")
-            self.model.fit(x_train, y_train, eval_set=[(x_val, y_val)], verbose=True)
+            self._model.fit(x_train, y_train, eval_set=[(x_val, y_val)], verbose=True)
         else:
             _LOGGER.info("Fitting the model without validation data")
-            self.model.fit(x_train, y_train, verbose=False)
+            self._model.fit(x_train, y_train, verbose=False)
 
     def predict_horizon(self, df: pd.DataFrame) -> np.ndarray:
         _check_data_format(df)
         transformed_test_data = CovariatesGenerator().transform(df)
         _LOGGER.info("Predicting the horizon")
         x_test, y_test = self.prepare_data(transformed_test_data)
-        return self.model.predict(x_test)
+        return self._model.predict(x_test)
 
     def predict_last(self, df: pd.DataFrame) -> np.ndarray:
         _check_data_format(df)
         transformed_test_data = CovariatesGenerator().transform(df)
         _LOGGER.info("Predicting the last value")
         x_test, y_test = self.prepare_data(transformed_test_data)
-        return self.model.predict(x_test[-1].reshape(1, -1))
+        return self._model.predict(x_test[-1].reshape(1, -1))
 
     def save_artifacts(self, path: str) -> None:
-        artifact = {"model": self.model}
+        artifact = {"model": self._model}
         torch.save(artifact, path)
-        _LOGGER.info(f"Model saved at {path}")
+        _LOGGER.info("Model saved at %s", path)
 
     def load_artifacts(self, path: str) -> None:
         artifact = torch.load(path)
-        self.model = artifact["model"]
+        self._model = artifact["model"]
         _LOGGER.info(f"Model loaded from {path}")
